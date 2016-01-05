340e80b1cdeb364ec8612104aa875c73cbd27494
kenobob <kenobob.github@gmail.com>
2016-01-04 21:56:09 -0700

    Added in base file
    Added in the base file generated from SmartThings IDE

---
+++ devicetypes/better-weather-station/better-weather-station.groovy
@@ -0,0 +1,63 @@
+/**
+ *  Better Weather Station
+ *
+ *  Copyright 2016 kenobob
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ *  in compliance with the License. You may obtain a copy of the License at:
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software distributed under the License is distributed
+ *  on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License
+ *  for the specific language governing permissions and limitations under the License.
+ *
+ */
+metadata {
+	definition (name: "Better Weather Station", namespace: "kenobobbws", author: "kenobob") {
+		capability "Configuration"
+		capability "Illuminance Measurement"
+		capability "Polling"
+		capability "Refresh"
+		capability "Relative Humidity Measurement"
+		capability "Sensor"
+		capability "Temperature Measurement"
+		capability "Water Sensor"
+	}
+
+	simulator {
+		// TODO: define status and reply messages here
+	}
+
+	tiles {
+		// TODO: define your main and details tiles here
+	}
+}
+
+// parse events into attributes
+def parse(String description) {
+	log.debug "Parsing '${description}'"
+	// TODO: handle 'illuminance' attribute
+	// TODO: handle 'humidity' attribute
+	// TODO: handle 'temperature' attribute
+	// TODO: handle 'water' attribute
+
+}
+
+// handle commands
+def configure() {
+	log.debug "Executing 'configure'"
+	// TODO: handle 'configure' command
+}
+
+def poll() {
+	log.debug "Executing 'poll'"
+	// TODO: handle 'poll' command
+}
+
+def refresh() {
+	log.debug "Executing 'refresh'"
+	// TODO: handle 'refresh' command
+}
+
+
