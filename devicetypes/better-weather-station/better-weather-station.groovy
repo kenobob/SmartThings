2267b17e67d55d1853b87befc51fba80692f49f5
kenobob <kenobob.github@gmail.com>
2016-01-05 01:01:43 -0700

    Formatted the code

--- devicetypes/better-weather-station/better-weather-station.groovy
+++ devicetypes/better-weather-station/better-weather-station.groovy
@@ -14,84 +14,84 @@
  *
  */
 metadata {
-	definition (name: "Better Weather Station", namespace: "kenobobbws", author: "kenobob") {
-		capability "Configuration"
-		capability "Illuminance Measurement"
-		capability "Polling"
-		capability "Refresh"
-		capability "Relative Humidity Measurement"
-		capability "Sensor"
-		capability "Temperature Measurement"
-		capability "Water Sensor"
-	}
+    definition (name: "Better Weather Station", namespace: "kenobobbws", author: "kenobob") {
+        capability "Configuration"
+        capability "Illuminance Measurement"
+        capability "Polling"
+        capability "Refresh"
+        capability "Relative Humidity Measurement"
+        capability "Sensor"
+        capability "Temperature Measurement"
+        capability "Water Sensor"
+    }
 
-	simulator {
-		// TODO: define status and reply messages here
-	}
+    simulator {
+        // TODO: define status and reply messages here
+    }
     
 
-	preferences {
-		input "zipCode", "text", title: "Zip Code", required: false
-	}
+    preferences {
+        input "zipCode", "text", title: "Zip Code", required: false
+    }
 
-	tiles {
+    tiles {
         valueTile("temperature", "device.temperature") {
-                state "default", label:'${currentValue}°',
-                    backgroundColors:[
-                    //Put in Color range for Northern United States Temperature Ranges.
-                        [value: -50, color: "#FFFFFF"],
-                        [value: -15, color: "#EF8CEF"],
-                        [value:  0, color: "#AE1EB8"],
-                        [value: 15, color: "#4B12A0"],
-                        [value: 31, color: "#153591"],
-                        [value: 44, color: "#1e9cbb"],
-                        [value: 59, color: "#90d2a7"],
-                        [value: 74, color: "#44b621"],
-                        [value: 84, color: "#f1d801"],
-                        [value: 95, color: "#d04e00"],
-                        [value: 96, color: "#bc2323"]
-                    ]
+            state "default", label:'${currentValue}°',
+            backgroundColors:[
+                //Put in Color range for Northern United States Temperature Ranges.
+                [value: -50, color: "#FFFFFF"],
+                [value: -15, color: "#EF8CEF"],
+                [value:  0, color: "#AE1EB8"],
+                [value: 15, color: "#4B12A0"],
+                [value: 31, color: "#153591"],
+                [value: 44, color: "#1e9cbb"],
+                [value: 59, color: "#90d2a7"],
+                [value: 74, color: "#44b621"],
+                [value: 84, color: "#f1d801"],
+                [value: 95, color: "#d04e00"],
+                [value: 96, color: "#bc2323"]
+            ]
         }
         
         main "temperature"
         details(["temperature"])
-	}
+    }
 }
 
 // parse events into attributes
 def parse(String description) {
-	log.debug("Parsing '${description}'")
-	// TODO: handle 'illuminance' attribute
-	// TODO: handle 'humidity' attribute
-	// TODO: handle 'temperature' attribute
-	// TODO: handle 'water' attribute
+    log.debug("Parsing '${description}'")
+    // TODO: handle 'illuminance' attribute
+    // TODO: handle 'humidity' attribute
+    // TODO: handle 'temperature' attribute
+    // TODO: handle 'water' attribute
 
 }
 
 //When the Device is installed, fire off this function. Note, does not fire if upgraded.
 def installed() {
-	log.trace("Executing 'installed'");
+    log.trace("Executing 'installed'");
     //TODO: Handle if Scheduler Dies.
     //TODO: Create Update Command to make user configurable
-	runPeriodically(90, poll)
-	log.trace("End Executing 'installed'");
+    runPeriodically(90, poll)
+    log.trace("End Executing 'installed'");
 }
 
 //When the Device is uninstalled, fire off this function
 def uninstalled() {
-	log.trace("Executing 'uninstalled'");
-	unschedule()
-	log.trace("End Executing 'uninstalled'");
+    log.trace("Executing 'uninstalled'");
+    unschedule()
+    log.trace("End Executing 'uninstalled'");
 }
 
 // handle commands
 def configure() {
-	log.debug("Executing 'configure'")
-	// TODO: handle 'configure' command
+    log.debug("Executing 'configure'")
+    // TODO: handle 'configure' command
 }
 
 def poll() {
-	log.trace("Executing 'poll'")
+    log.trace("Executing 'poll'")
     def weatherConditions
     
     //Grab the appropriate weather based on user's imput
@@ -109,22 +109,22 @@ def poll() {
     	def obs = weatherConditions.current_observation
     	//WU sent information.
         if(getTemperatureScale() == "C") {
-			sendEvent(name: "temperature", value: obs.temp_c, unit: "C")
-		} else {
-    		log.debug( "Current Temperature: ${obs.temp_f}ºF" )
-			sendEvent(name: "temperature", value: obs.temp_f, unit: "F")
-		}
+            sendEvent(name: "temperature", value: obs.temp_c, unit: "C")
+        } else {
+            log.debug( "Current Temperature: ${obs.temp_f}ºF" )
+            sendEvent(name: "temperature", value: obs.temp_f, unit: "F")
+        }
     } else {
     	//Weather Underground did not return any weather information.
     	log.warn("Unable to get current weather conditions from Weather Underground API.")
     }
     
-	log.trace("End Executing 'poll'")
+    log.trace("End Executing 'poll'")
 }
 
 def refresh() {
-	log.trace("Executing 'refresh'")
+    log.trace("Executing 'refresh'")
     //Manually re-trigger the polling event.
     poll()
-	log.trace("End Executing 'refresh'")
+    log.trace("End Executing 'refresh'")
 }
