616e39bedc2124a3e93d9e731162b65273f71df7
kenobob <kenobob.github@gmail.com>
2016-02-02 23:47:37 -0700

    Stubbed out basics for new block heater smart app.

---
+++ smartapps/kenobob/smart-block-heater.src/smart-block-heater.groovy
@@ -0,0 +1,62 @@
+/**
+ *  Smart Block Heater
+ *
+ *  Copyright 2016 kenobob
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ *  in compliance with the License. You may obtain a copy of the License at:
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software distributed under the License is distributed
+ *  on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License
+ *  for the specific language governing permissions and limitations under the License.
+ *
+ */
+definition(
+    name: "Smart Block Heater",
+    namespace: "kenobob",
+    author: "kenobob",
+    description: "This will be a smart block heater",
+    category: "My Apps",
+    iconUrl: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience.png",
+    iconX2Url: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience@2x.png",
+    iconX3Url: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience@2x.png")
+
+
+preferences {
+    section("For this amount of time") {
+        input "minutes", "number", title: "Minutes?"
+    }
+    section("After this time of day") {
+        input "timeOfDay", "time", title: "Time?"
+    }
+    section("And (optionally) these switches are all off") {
+        input "switches", "capability.switch", multiple: true, required: false
+    }
+    section( "Notifications" ) {
+        input("recipients", "contact", title: "Send notifications to") {
+            input "sendPushMessage", "enum", title: "Send a push notification?", options: ["Yes", "No"], required: false
+            input "phoneNumber", "phone", title: "Send a Text Message?", required: false
+        }
+    }
+}
+
+def installed() {
+    log.debug "Installed with settings: ${settings}"
+
+    initialize()
+}
+
+def updated() {
+    log.debug "Updated with settings: ${settings}"
+
+    unsubscribe()
+    initialize()
+}
+
+def initialize() {
+    // TODO: subscribe to attributes, devices, locations, etc.
+}
+
+// TODO: implement event handlers
