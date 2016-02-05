fe7ffe26d363d03a0e8f4283ef28fe947e58c70f
kenobob <kenobob.github@gmail.com>
2016-02-05 00:32:38 -0700

    first attempt to make scheduler.

--- smartapps/kenobob/smart-block-heater.src/smart-block-heater.groovy
+++ smartapps/kenobob/smart-block-heater.src/smart-block-heater.groovy
@@ -28,9 +28,15 @@ preferences {
     section("On for this amount of time") {
         input (name: "minutes", type: "number", title: "Minutes?", required: true)
     }
+    section("Temperature You want to turn on at.") {
+        input (name: "onTemperature", type: "number", title: "Minutes?", required: true)
+    }
     //Time or Mode, not sure yet.
     section("When does your quiet hours start?") {
-        input "timeOfDay", "time", title: "Time?", required: true
+        input "beforeBedNotificationTime", "time", title: "Time?", required: true
+    }
+    section("When do you need your car to start?") {
+        input "carStartTime", "time", title: "Time?", required: true
     }
     section("Which Switch is your block heater plugged into?") {
         input "switches", "capability.switch", multiple: true, required: true
@@ -70,7 +76,10 @@ def initialize() {
 private def createSubscriptions()
 {
     log.trace("Executing Create Subscriptions")
-    subscribe(bwsTemperatureMeasurement, "lowtemperature", temperatureChanges)
+    
+    //Subscribe to BWS low tempurature change.
+    subscribe(bwsTemperatureMeasurement, "lowtemperature", lowForecastedTemperatureChanges)
+    
     //	subscribe(motionSensors, "motion.active", motionActiveHandler)
     //	subscribe(motionSensors, "motion.inactive", motionInactiveHandler)
     //	subscribe(switches, "switch.off", switchOffHandler)
@@ -84,8 +93,44 @@ private def createSubscriptions()
 
 
 // TODO: implement event handlers
-def temperatureChanges(evt){
+def lowForecastedTemperatureChanges(evt){
     
-    log.trace("Executing Create Subscriptions")
-    log.debug "The Low Changed To: ${evt.numericValue}"
+    log.trace("Executing lowForecastedTemperatureChanges")
+    log.debug ("The Low Changed To: ${evt.numericValue}")
+    
+    if(evt.numericValue <= onTemperature){
+        //The low tempurature is going to be cold enough we want to turn on switch.
+        log.info("Forecast Low is going to be below threshold")
+        checkCreateScheduler()
+    } else {
+        log.info("Nice and warm, no worries.")
+    }
+    
+    
+    log.trace("End lowForecastedTemperatureChanges")
+}
+
+private def checkCreateScheduler(){
+    log.trace("Executing checkCreateScheduler")
+    //I'm out of scheduled events somehow, clear them out!
+    if(!canSchedule()){
+        log.debug("Scheduler Full, clear them out and let's start over")
+    }
+    
+    log.debug("Set Notification time for ${beforeBedNotificationTime}")
+    //Create Reminder to Plug in Car.
+//    def tempbeforeBedNotificaitonDate = new Date()
+//    tempbeforeBedNotificaitonDate.set( hourOfDay: 12, minute: 0, second: 0)
+
+    //Doesn't work.... 
+    runOnce(beforeBedNotificationTime, notifyUserToPlugIn)
+    log.trace("End checkCreateScheduler")
+}
+
+private def notifyUserToPlugIn(){
+    log.trace("Executing notifyUserToPlugIn")
+    if(sendPushMessage != null && sendPushMessage){
+        sendNotificaiton("Plug in your block heater.", [method: "push"])
+    }
+    log.trace("End notifyUserToPlugIn")
 }
