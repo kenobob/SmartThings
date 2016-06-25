5fe458da8d203a414d2c5ea3c48ffedf4550dbbe
kenobob <kenobob.github@gmail.com>
2016-06-24 23:47:38 -0600

    Added header comment

--- smartapps/kenobob/better-virtual-thermostat.src/better-virtual-thermostat.groovy
+++ smartapps/kenobob/better-virtual-thermostat.src/better-virtual-thermostat.groovy
@@ -1,17 +1,19 @@
+
 /**
-*  Virtual Thermostat
-* TODO: 
-Fix Over Evaluating from certain triggers
-Fix to Pull from real switch(s) state instead of assumed state
-Make Mode's Dynamic, default to 3, but could add more
-Window Sensors & Open Time Before Turning off?
-Outside Temperature
-Forecast
-Disable Switch/Modes/andor setting
-Primary Sensor For Temp & All Sensors no greater than...
-Learning - Schedule when coming home, etc
-*
-*/
+ *  Virtual Thermostat
+ *
+ *  Copyright 2016 kenobob
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ *  in compliance with the License. You may obtain a copy of the License at:
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software distributed under the License is distributed
+ *  on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License
+ *  for the specific language governing permissions and limitations under the License.
+ *
+ */
 definition(
     name: "Better Virtual Thermostat",
     namespace: "kenobob",
